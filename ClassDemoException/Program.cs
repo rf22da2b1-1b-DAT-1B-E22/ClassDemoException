a9c13238fa900ace14e658090ada50b575609af3
Peter Levinsky <pele@zealand.dk>
2022-11-03 13:02:49 +0100

    Add project files.

---
+++ ClassDemoException/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoException.model;
+
+Console.WriteLine("Hello, World!");
+
+
+Vare defaultVare = new Vare(); // brug af default konstruktør
+Vare cola = new Vare("Cola", 13.50, 55);
+
+Console.WriteLine(defaultVare);
+Console.WriteLine(cola);
+
+try
+{
+    cola.Pris = -10;
+}catch(ArgumentException ae)
+{
+    // behandle fejl
+    Console.WriteLine("Der er en fejl: " + ae.Message);
+}
+
+
+try
+{
+    cola.Navn = null;
+}
+catch(ArgumentException ae)
+{
+    Console.WriteLine("Der er en fejl: " + ae.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
---
+++ ClassDemoException/model/Vare.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoException.model
+{
+    public class Vare
+    {
+        // instans felter
+        private string _navn;
+        private double _pris;
+        private int _antalPåLager;
+
+
+        // properties
+        public string Navn
+        {
+            get { return _navn; }
+            set { 
+
+                if (value is null || value.Length < 4)
+                {
+                    throw new ArgumentException("navnet skal være over 3 tre tegn langt");
+                }
+                _navn = value; 
+            }
+        }
+
+        public double Pris { 
+            get { return _pris; }
+            set { 
+                if (value < 0)
+                {
+                    throw new ArgumentException("Prisen må ikke være negativ");
+                }
+                _pris = value; 
+            }
+        }
+
+        public int AntalPåLager
+        {
+            get { return _antalPåLager; }
+            set {
+
+                if (value >= 0)
+                {
+                    _antalPåLager = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Vi kan ikke have et negativt lager");
+                }
+            }
+        }
+
+
+
+        // konstruktør
+        public Vare():this("dummy", 0, 0)
+        {
+
+        }
+
+        public Vare(string navn, double pris, int lager)
+        {
+            _navn = navn;
+            _pris = pris;
+            _antalPåLager = lager;
+        }
+
+
+
+
+        // to string
+        public override string ToString()
+        {
+            return $"Navn={_navn} Pris={_pris} varer på lager={_antalPåLager}";
+        }
+    }
+}
